73fb2f74d3f7521849dff4e1e611636dca8c6f4e
newtonkiragu <karanunewton4@gmail.com>
2017-09-05 10:53:46 +0300

    did the routing for the index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'home#index'
-  root 'home#index'
+  root 'homes#index'
+  get 'homes/index'
 end
