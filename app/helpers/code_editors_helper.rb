c7ea25334fe0d3127fd840782f9dd971874186aa
pmutua <pmutua@live.com>
2017-09-05 14:14:45 +0300

    added controller,update routes + added ACE  embedded code editor

---
+++ app/helpers/code_editors_helper.rb
@@ -0,0 +1,2 @@
+module CodeEditorsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :code_editors
   resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ 
   root 'homes#index'
   get 'homes/index'
+  get 'code_editors/index'
+
 end
