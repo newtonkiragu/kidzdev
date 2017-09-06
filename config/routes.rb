03fcb2584cc930084c9a74c664ce2b812ec22704
pmutua <pmutua@live.com>
2017-09-06 18:20:37 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :homes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :languages
+  resources :languages do
+    resources :lessons
+    
+  end
 
   get 'languages/index'
   root 'homes#index'
