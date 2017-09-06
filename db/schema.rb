75c4d31a2213374c86208bef5e9c0a70d8b0ec55
DennisKimani <dennisngugi.dk@gmail.com>
2017-09-06 14:46:32 +0300

    created the books database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905103608) do
+ActiveRecord::Schema.define(version: 20170906114306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
