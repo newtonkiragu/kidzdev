c62d666dd5185740bc51ebe23fb2e860b76fe241
pmutua <pmutua@live.com>
2017-09-07 17:36:59 +0300

    update views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories do
-    resources :lessons
+    resources :subcategories
     
   end
   
