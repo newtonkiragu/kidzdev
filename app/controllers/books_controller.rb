eb6066345d125bdd522da84a3af75f8239a03d60
DennisKimani <dennisngugi.dk@gmail.com>
2017-09-11 15:00:47 +0300

    permitted the images.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :context)
+    params.require(:book).permit(:title, :image, :context)
   end
 end
